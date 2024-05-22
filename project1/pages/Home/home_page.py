e74da9f414bf27bd83b88043092688c4c917b8df
Dasun Sooriyabandara <115138972+DasunSooriyabandara@users.noreply.github.com>
2024-05-22 23:39:40 +0530

    day three

---
+++ project1/pages/Home/home_page.py
@@ -0,0 +1,53 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+
+def click_downloads(driver):
+    wait = WebDriverWait(driver, 30)
+    downloads_link = wait.until(EC.element_to_be_clickable((By.XPATH, "//*[@id=\"downloads\"]")))
+    downloads_link.click()
+    print('~~~~~~ Downloads link Clicked')
+    print(str(driver.current_url))
+    return driver
+
+def search(driver,search_text):
+    wait = WebDriverWait(driver, 30)
+    search_feild = wait.until(EC.element_to_be_clickable((By.XPATH, "//*[@id=\"id-search-field\"]")))
+    search_feild.send_keys(search_text)
+    print('~~~~~~ Searching for Text')
+    GO_button = wait.until(EC.element_to_be_clickable((By.XPATH, "//*[@id=\"submit\"]")))
+    GO_button.click()
+    third_result = wait.until(EC.element_to_be_clickable((By.XPATH, "//*[@id=\"content\"]/div/section/form/ul/li[3]/h3/a")))  
+    if search_text == third_result.text :
+        print('Test Case Passed')
+    else:
+        print('Test Case Failed. The Searched result is'+ third_result.text)
+    print(str(driver.current_url))
+    return driver
+
+def search_by_looping(driver,search_text):
+    wait = WebDriverWait(driver, 30)
+    search_feild = wait.until(EC.element_to_be_clickable((By.XPATH, "//*[@id=\"id-search-field\"]")))
+    search_feild.send_keys(search_text)
+    print('~~~~~~ Searching for Text')
+    GO_button = wait.until(EC.element_to_be_clickable((By.XPATH, "//*[@id=\"submit\"]")))
+    GO_button.click()
+    for i in range(1, 21):
+        print(i)
+        try:
+            serch_xpath = "//*[@id=\"content\"]/div/section/form/ul/li["+str(i)+"]/h3/a"
+            search_result = wait.until(EC.element_to_be_clickable((By.XPATH,serch_xpath)))
+            print(search_result.text)
+            if search_text == search_result.text :
+                print('Match')
+                break
+            else:
+                print('No Math')
+        except:
+            print()
+    return driver
+
+
+
+    
--- project1/test_cases/test_case1.py
+++ project1/test_cases/test_case1.py
@@ -1,18 +1,49 @@
-import time
 from jproperties import Properties
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-# Create a Properties object
+import sys
+sys.path.append("E:\github\Selenium_Python\project1")
+from pages.Home.home_page import click_downloads , search , search_by_looping
+from pages.Downloads.downloads_page import click_download_python
+
+
+
 properties = Properties()
-# Load properties from file
+
 with open('testcase.properties', 'rb') as f:
     properties.load(f, 'utf-8')
-# Access values
+
 test_url = properties.get('test_url')[0]
 print(test_url)
-service = webdriver.ChromeService(executable_path = 'E:\github\Selenium_Python\project1\drivers\chromedriver.exe')
-driver = webdriver.Chrome(service=service)
-driver.get(test_url)
-driver.maximize_window()
-time.sleep(5)
-driver.close()
+
+def open_browser():
+    service = webdriver.ChromeService(executable_path = 'E:\github\Selenium_Python\project1\drivers\chromedriver.exe')
+    driver = webdriver.Chrome(service=service)
+    driver.maximize_window()
+    driver.get(test_url)
+    return driver
+
+
+def check_latest_python_version():
+    python_version_text = "Download Python 3.12.3"
+    driver = open_browser()
+    driver = click_downloads(driver)
+    driver = click_download_python(driver,python_version_text)
+    driver.close()
+
+def search_for_python_realses():
+    search_text = "Python 2.5 Release"
+    driver = open_browser()
+    driver = search_by_looping(driver,search_text)
+    driver.close()
+
+search_for_python_realses()
+
+
+
+
+
+
+
+
+
